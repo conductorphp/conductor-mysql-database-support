046d37e9b7da013a9efc7a4ef42921a2010f952f
Kirk <kmadera@robofirm.com>
2018-03-25 11:17:41 -0400

    Fixed handling of workingdir in mydumper export

--- src/Adapter/Mydumper/ExportPlugin.php
+++ src/Adapter/Mydumper/ExportPlugin.php
@@ -74,11 +74,11 @@ class ExportPlugin
         $this->assertIsUsable();
         $this->validateOptions($options);
 
-        $command = $this->getMyDumperExportCommand($database, $workingDir, $options);
+        $command = $this->getMyDumperExportCommand($database, $options);
 
         try {
             $this->shellAdapter->runShellCommand($command, null, null, ShellAdapterInterface::PRIORITY_LOW);
-            $this->shellAdapter->runShellCommand('pwd && rm -rf ' . escapeshellarg($database));
+            $this->shellAdapter->runShellCommand('pwd && rm -rf ' . escapeshellarg($database), $workingDir);
 
         } catch (\Exception $e) {
             throw new Exception\RuntimeException($e->getMessage());
@@ -89,28 +89,27 @@ class ExportPlugin
 
     /**
      * @param string $database
-     * @param string $workingDir
      * @param array  $options
      *
      * @return string
      */
-    private function getMyDumperExportCommand(string $database, string $workingDir, array $options): string
+    private function getMyDumperExportCommand(string $database, array $options): string
     {
         # find command has a bug where it will fail if you do not have read permissions to the current working directory
         # Temporarily switching into the working directory while running this command.
         # @link https://unix.stackexchange.com/questions/349894/can-i-tell-find-to-to-not-restore-initial-working-directory
 
         $dumpStructureCommand = 'mydumper --database ' . escapeshellarg($database) . ' --outputdir '
-            . escapeshellarg($workingDir) . ' -v 3 --no-data --triggers --events --routines --less-locking '
+            . escapeshellarg($database) . ' -v 3 --no-data --triggers --events --routines --less-locking '
             . $this->getMysqldumperCommandConnectionArguments() . ' ';
 
         if (!empty($options[self::OPTION_REMOVE_DEFINERS])) {
-            $dumpStructureCommand .= '&& cd ' . escapeshellarg($workingDir)
-                . ' && find . -name "*-schema-triggers.sql" -exec sed -ri \'s|DEFINER=[^ ]+ *||g\' {} \;';
+            $dumpStructureCommand .= '&& find ' . escapeshellarg($database)
+                . ' -name "*-schema-triggers.sql" -exec sed -ri \'s|DEFINER=[^ ]+ *||g\' {} \;';
         }
 
         $dumpDataCommand = 'mydumper --database ' . escapeshellarg($database) . ' --outputdir '
-            . escapeshellarg($workingDir) . ' -v 3 --no-schemas --less-locking '
+            . escapeshellarg($database) . ' -v 3 --no-schemas --less-locking '
             . $this->getMysqldumperCommandConnectionArguments() . ' ';
 
         $dataTables = $this->getDataTables($database, $options);
@@ -118,14 +117,14 @@ class ExportPlugin
             $dumpDataCommand .= '--tables-list ' . implode(',', $dataTables);
         }
 
+        // @todo Move this to somewhere else. This is specific to a known Magento issue
         // Avoid issue with tables that defaults timestamp fields to '0000-00-00 00:00:00', which cause on error on
         // import
-        $fixTimestampDefaultIssueCommand = 'cd ' . escapeshellarg($workingDir)
-            . ' && find . -name "*.sql" -exec sed -ri "s|(timestamp\|datetime) (NOT )?NULL DEFAULT '
+        $fixTimestampDefaultIssueCommand = 'find ' .  escapeshellarg($database)
+            . ' -name "*.sql" -exec sed -ri "s|(timestamp\|datetime) (NOT )?NULL DEFAULT '
             . '\'0000-00-00 00:00:00\'|\1 \2NULL DEFAULT CURRENT_TIMESTAMP|gI" {} \;';
 
-        $tarCommand = 'cd ' . escapeshellarg(dirname($workingDir)) . ' && '
-            . 'tar czf ' . escapeshellarg("$database.tgz") . ' ' . escapeshellarg(basename($workingDir));
+        $tarCommand = 'tar -czf ' . escapeshellarg("$database.tgz") . ' ' . escapeshellarg($database);
 
         return "$dumpStructureCommand && $dumpDataCommand && $fixTimestampDefaultIssueCommand && $tarCommand";
     }
